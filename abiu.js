b1c75b8a1a57f0fe0d1dacff4b28d28ce97f3189
Igor Entaltsev <igor.yentaltsev@gmail.com>
2013-06-05 14:54:14 -0400

    130605 14:54 added more modules

--- abiu.js
+++ abiu.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var app = express();                   // 3
+var dirname = process.cwd();
+var path = require('path');            // 12
 
 
 /*
@@ -34,13 +36,17 @@ app.listen(3000);                               // 8
 
 
 //============================================
-// 1) executes 2nd, when user hits the site
-//    here the res object is augmented by 'express'. send() is one of the higher level methods. send() adds the Content-Length for you.
-// 2) executes 1st, when you do "node app.js" 
-// 3) creating a new application instance called 'app' with 'express'
-// 4) app.VERB() is how you define "routes"
-// 5) this string will come out in response to "GET/" 
-// 6) "req" and "res" - the exact same objects that Node provides to you 
-// 7) invoke a function on the res object
-// 8) invoking express.listen() to bind and listen to connections
-// 9) error-handling middleware must be defined with an arity of 4 (like this)
+/* 
+   1) executes 2nd, when user hits the site    
+      here the res object is augmented by 'express'. send() is one of the higher level methods. send() adds the Content-Length for you.
+   2) executes 1st, when you do "node app.js"  
+   3) creating a new application instance called 'app' with 'express'
+   4) app.VERB() is how you define "routes"
+   5) this string will come out in response to "GET/" 
+   6) "req" and "res" - the exact same objects that Node provides to you 
+   7) invoke a function on the res object
+   8) invoking express.listen() to bind and listen to connections
+   9) error-handling middleware must be defined with an arity of 4 (like this)
+ 
+   12) module required for handling and transforming file paths.
+ */
